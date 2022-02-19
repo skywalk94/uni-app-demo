c382e6147b8e859c577661ce3d0613f70316bcef
skywalk94 <skywalk94@foxmail.com>
2022-02-19 16:44:20 +0800

    省市区联动

--- App.vue
+++ App.vue
@@ -1,17 +1,6 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
+	export default {}
 </script>
 
 <style lang="scss">
-	@import "@/uni_modules/uview-ui/index.scss";
 </style>
--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 import App from './App'
 import Vue from 'vue'
-import uView from '@/uni_modules/uview-ui'
-Vue.use(uView)
 Vue.config.productionTip = false
 App.mpType = 'app'
 
